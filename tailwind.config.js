078a329c9330aac4c22a29ac0acff547ea86a9fe
Yordan Yordanov <yoyordanov94@gmail.com>
2025-08-06 18:19:51 +0300

    change zinc tailwind based on theme.css configuration (flux related)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-import colors from "tailwindcss/colors.js";
-
 export default {
     darkMode: 'selector',
 
@@ -17,7 +15,22 @@ export default {
                 sans: ['Inter', 'sans-serif'],
             },
             colors: {
-                zinc: colors.slate,
+                // Theme-aware zinc colors using RGB space for CSS variables
+                zinc: {
+                    50: 'rgb(var(--zinc-50) / <alpha-value>)',
+                    100: 'rgb(var(--zinc-100) / <alpha-value>)',
+                    200: 'rgb(var(--zinc-200) / <alpha-value>)',
+                    300: 'rgb(var(--zinc-300) / <alpha-value>)',
+                    400: 'rgb(var(--zinc-400) / <alpha-value>)',
+                    500: 'rgb(var(--zinc-500) / <alpha-value>)',
+                    600: 'rgb(var(--zinc-600) / <alpha-value>)',
+                    700: 'rgb(var(--zinc-700) / <alpha-value>)',
+                    800: 'rgb(var(--zinc-800) / <alpha-value>)',
+                    900: 'rgb(var(--zinc-900) / <alpha-value>)',
+                    950: 'rgb(var(--zinc-950) / <alpha-value>)',
+                },
+
+                // Theme compliment colors
                 compliment: {
                     DEFAULT: 'var(--color-compliment)',
                     content: 'var(--color-compliment-content)',
