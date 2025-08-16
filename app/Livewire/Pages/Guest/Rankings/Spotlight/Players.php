f9b6574690acb31f9040b4da50ea19a16bfeddd2
Yordan Yordanov <yoyordanov94@gmail.com>
2025-08-16 15:07:35 +0300

    add rf and sum for s6 (in players spotlight)

--- app/Livewire/Pages/Guest/Rankings/Spotlight/Players.php
+++ app/Livewire/Pages/Guest/Rankings/Spotlight/Players.php
@@ -2,9 +2,11 @@
 
 namespace App\Livewire\Pages\Guest\Rankings\Spotlight;
 
+use App\Enums\Game\ServerVersion;
 use App\Livewire\BaseComponent;
 use App\Models\Game\Character;
 use App\Models\Game\Ranking\HallOfFame;
+use App\Models\Utility\GameServer;
 use Illuminate\Support\Facades\Cache;
 use Livewire\Attributes\Computed;
 
@@ -16,6 +18,8 @@ class Players extends BaseComponent
         'fe' => ['class_name' => 'Elves', 'color' => 'green'],
         'mg' => ['class_name' => 'Gladiators', 'color' => 'purple'],
         'dl' => ['class_name' => 'Lords', 'color' => 'yellow'],
+        'sum' => ['class_name' => 'Summoners', 'color' => 'cyan'],
+        'rf' => ['class_name' => 'Fighters', 'color' => 'orange'],
     ];
 
     public const DEFAULT_WINNERS = [
@@ -24,6 +28,8 @@ class Players extends BaseComponent
         'fe' => 'TBD',
         'mg' => 'TBD',
         'dl' => 'TBD',
+        'sum' => 'TBD',
+        'rf' => 'TBD',
     ];
 
     public const COLOR_CLASSES = [
@@ -52,21 +58,34 @@ class Players extends BaseComponent
             'border' => 'border-yellow-600/90 dark:border-yellow-400/30',
             'text' => 'text-yellow-600 dark:text-yellow-400',
         ],
+        'cyan' => [
+            'gradient' => 'from-cyan-700 via-cyan-600 to-transparent dark:from-cyan-600 dark:via-cyan-700',
+            'border' => 'border-cyan-600/90 dark:border-cyan-400/30',
+            'text' => 'text-cyan-600 dark:text-cyan-400',
+        ],
+        'orange' => [
+            'gradient' => 'from-orange-700 via-orange-600 to-transparent dark:from-orange-600 dark:via-orange-700',
+            'border' => 'border-orange-600/90 dark:border-orange-400/30',
+            'text' => 'text-orange-600 dark:text-orange-400',
+        ],
     ];
 
     #[Computed]
     public function winners(): array
     {
         $connection = session('game_db_connection', 'gamedb_main');
+        $server = GameServer::default();
+        $serverVersion = $server?->server_version ?? ServerVersion::Season3;
 
-        return Cache::remember("hall_of_fame_winners_{$connection}", now()->addWeek(), function () {
+        return Cache::remember("hall_of_fame_winners_{$connection}_{$serverVersion->value}", now()->addWeek(), function () use ($serverVersion) {
+            $availableClasses = $this->getAvailableClasses($serverVersion);
             $latestWinners = HallOfFame::first();
 
             // If no HallOfFame data, return defaults
             if (! $latestWinners) {
-                return collect(self::DEFAULT_WINNERS)->map(function ($name, $class) {
+                return collect($availableClasses)->map(function ($class) {
                     return [
-                        'name' => $name,
+                        'name' => self::DEFAULT_WINNERS[$class],
                         'class' => $class,
                         'class_name' => self::CHARACTER_CONFIG[$class]['class_name'],
                         'color' => self::CHARACTER_CONFIG[$class]['color'],
@@ -77,7 +96,7 @@ class Players extends BaseComponent
             }
 
             // Get actual winners
-            return collect(self::CHARACTER_CONFIG)->map(function ($config, $class) use ($latestWinners) {
+            return collect($availableClasses)->map(function ($class) use ($latestWinners) {
                 $characterName = $latestWinners->$class ?: self::DEFAULT_WINNERS[$class];
 
                 $character = Character::where('Name', $characterName)->first();
@@ -85,13 +104,22 @@ class Players extends BaseComponent
                 return [
                     'name' => $characterName,
                     'class' => $class,
-                    'class_name' => $config['class_name'],
-                    'color' => $config['color'],
+                    'class_name' => self::CHARACTER_CONFIG[$class]['class_name'],
+                    'color' => self::CHARACTER_CONFIG[$class]['color'],
                     'image' => "images/characters/{$class}.png",
                     'hof_wins' => $character?->HofWins ?? 0,
                 ];
             })->values()->toArray();
         });
+    }
+
+    private function getAvailableClasses(ServerVersion $serverVersion): array
+    {
+        return match ($serverVersion) {
+            ServerVersion::Season6 => ['dk', 'dw', 'fe', 'mg', 'dl', 'sum', 'rf'],
+            ServerVersion::Season3 => ['dk', 'dw', 'fe', 'mg', 'dl'],
+            default => ['dk', 'dw', 'fe', 'mg', 'dl'],
+        };
     }
 
     protected function getViewName(): string
