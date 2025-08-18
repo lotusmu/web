26a365e940f6d40592e091a59c0af59975ecc366
Yordan Yordanov <yoyordanov94@gmail.com>
2025-08-18 18:30:31 +0300

    wip

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
-    App\Providers\ThemeServiceProvider::class,
+    // App\Providers\ThemeServiceProvider::class,  // Temporarily disabled until database is fixed
     App\Providers\VoltServiceProvider::class,
 ];
